aa58c2512eb3a9c8c85406b7328b5ea60f93eee7
Agnieszka Falandys <agnfal@gmail.com>
2021-10-18 10:04:06 +0200

    WebSocket

--- client/app.js
+++ client/app.js
@@ -5,9 +5,24 @@ const addMessageForm = document.querySelector('#add-messages-form');
 const userNameInput = document.querySelector('#username');
 const messageContentInput = document.querySelector('#message-content');
 
+const socket = io();
+
 let userName;
 
 messagesSection.classList.remove('show'); 
+
+const addMessage = function(author, content){ 
+    const message = document.createElement('li');
+    message.classList.add('message');
+    message.classList.add('message--received');
+
+    author === userName ? message.classList.add('message--self') : ( author === 'ChatBot' ? message.classList.add('message--chatbot'): '');
+
+    message.innerHTML = `<h3 class="message__author">${author === userName ? 'You' : author}</h3>
+                         <div class="message__content">${content}</div>`;
+
+    messagesList.appendChild(message);
+}
 
 const login = function(event){
     if( userNameInput.value == '') alert('Username id empty!');
@@ -15,25 +30,17 @@ const login = function(event){
         userName = userNameInput.value;
         loginForm.classList.remove('show');
         messagesSection.classList.add('show');
+        socket.emit('user',{login: userName});
+        socket.emit('message',{author: 'ChatBot', content: userName + ' has joined the conversation!'});
     }
 }
 
+socket.on('message', (message) => addMessage(message.author,message.content));
+
 const sendMessage = function(event){
    messageContentInput.value == '' ? alert('Message is empty!'): addMessage(userName, messageContentInput.value);
+   socket.emit('message',{author: userName, content: messageContentInput.value});
    messageContentInput.value = '';
-}
-
-const addMessage = function(author, content){
-    const message = document.createElement('li');
-    message.classList.add('message');
-    message.classList.add('message--received');
-
-    author === userName ? message.classList.add('message--self') : '';
-
-    message.innerHTML = `<h3 class="message__author">${author === userName ? 'You' : author}</h3>
-                         <div class="message__content">${content}</div>`;
-
-    messagesList.appendChild(message);
 }
 
 loginForm.addEventListener('submit', (event) => {
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+const { Console } = require('console');
 const express = require('express');
 const path = require('path');
+const socket = require('socket.io');
 
 const app = express();
 
 const messages = [];
+const users = [];
 
 app.use(express.static(path.join(__dirname, '/client')));
 
@@ -11,6 +14,29 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/index.html'));
 });
 
-app.listen(8000, () => {
+const server = app.listen(8000, () => {
   console.log('Server is running on port: 8000');
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => { 
+  socket.on('user', (user)=> { 
+    if(typeof user.login !== 'undefined') {
+      users.push({name:user.login, id:socket.id}); console.log('users',users);
+    }
+  });
+
+  socket.on('message', (message) => {
+    messages.push(message);
+    socket.broadcast.emit('message', message);
+  });
+
+  socket.on('disconnect', () => { console.log('tabl',users); console.log('socket',socket.id);
+    const index = users.findIndex(el => el.id == socket.id); console.log('index',index);
+    if(index !== -1) { console.log(users[index].name);
+      socket.broadcast.emit('message', {author:'ChatBot', content: users[index].name + ' has left the conversation... :('});
+      users.splice(index,1);
+    }
+  });
 });
